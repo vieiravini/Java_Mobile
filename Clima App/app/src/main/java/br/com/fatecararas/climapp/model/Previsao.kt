5803c352f7b5a44c1083ed0c5c7b03f93763bd2a
Vinicius Vieira <51539646+vieiravini@users.noreply.github.com>
2019-11-22 21:57:50 -0300

    Add files via upload

---
+++ Clima App/app/src/main/java/br/com/fatecararas/climapp/model/Previsao.kt
@@ -0,0 +1,29 @@
+package br.com.fatecararas.climapp.model
+
+import java.io.Serializable
+
+class Previsao : Serializable {
+
+    var data: String? = null
+    var diaDaSemana: String? = null
+    var maxima: String? = null
+    var minima: String? = null
+    var descricao: String? = null
+    var condicao: String? = null
+
+    constructor(
+        data: String, diaDaSemana: String, maxima: String, minima: String, descricao: String, condicao: String) {
+        this.data = data
+        this.diaDaSemana = diaDaSemana
+        this.maxima = maxima
+        this.minima = minima
+        this.descricao = descricao
+        this.condicao = condicao
+    }
+
+    constructor() {}
+
+    companion object {
+        private const val serialVersionUID = 1L
+    }
+}
